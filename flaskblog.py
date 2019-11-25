bb6eb108ce057d88c20616b82785985f73f9b46a
Celmad <manuelalaminosd@protonmail.com>
2019-11-25 23:12:12 +0000

    Added Login and Register pages. Tested with mock data

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
-from flask import Flask, render_template, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f04516615517d8dbf6f9e4392faeec0e'
 
 posts = [
     {
@@ -26,6 +28,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST']) # allow these methods
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success') # second argument is category, passing a bootstrap 4 class
+        return redirect(url_for('home')) # passing name of function, not of route
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login unsuccessful. Please check username and password', 'danger ')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
