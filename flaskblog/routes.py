fdd00b88ae61b3f0a0e9196c7b62551df77afb34
Celmad <manuelalaminosd@protonmail.com>
2019-11-29 00:14:10 +0000

    Function to add profile picture implemented

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, flash, redirect
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -59,7 +62,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             # login_user accepts a second argument, we will pass the 'remember me' checkbox value
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Unsuccessful. Email or password might be incorrect. Please, try again.', 'danger ')
     return render_template('login.html', title='Login', form=form)
@@ -70,3 +74,38 @@ def logout():
     # Doesn't take any argument, cause it knows what user is logged in
     logout_user()
     return redirect(url_for('home'))
+
+
+def save_photo(form_photo):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_photo.filename)
+    photo_filename = random_hex + f_ext
+    photo_path = os.path.join(app.root_path, 'static/profile_photos', photo_filename)
+    output_size = (250, 250)
+    # resizing image
+    image = Image.open(form_photo)
+    image.thumbnail(output_size)
+    image.save(photo_path)
+    return photo_filename
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.profile_photo.data:
+            photo_file = save_photo(form.profile_photo.data)
+            current_user.photo_profile = photo_file
+        current_user.username = form.username.data)
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your profile has been updated', 'success')
+        return redirect(url_for('account')) # redirecting here so that the render template below doesn't try to submit form again
+    elif request.method == 'GET':
+        # to populate form with current data
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    photo_profile = url_for('static', filename='profile_photos/' + current_user.photo_profile)
+    return render_template('account.html', title='Account', photo_profile=photo_profile, form=form)
+
+
