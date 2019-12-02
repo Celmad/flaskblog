df3da1f590973147a5ad78963cad2c9d71153510
Celmad <manuelalaminosd@protonmail.com>
2019-12-02 19:39:50 +0000

    Added CSS variables to store colours. Dark Theme has been improved

--- flaskblog/static/script.js
+++ flaskblog/static/script.js
@@ -1,9 +1,10 @@
 const THEME_TOGGLE = document.getElementById('theme-toggle');
 const BODY = document.body;
+const SIDEBAR_ITEMS = document.getElementsByClassName('sidebar-items');
+const THEME_ICON = document.getElementById('theme-icon');
+const PAGE_HEADING = document.getElementById('page-heading');
 let articles = document.getElementsByClassName('content-section');
 let articlesTitle = document.getElementsByClassName('article-title');
-let sidebarItems = document.getElementsByClassName('sidebar-items');
-let themeIcon = document.getElementById('theme-icon');
 
 THEME_TOGGLE.addEventListener('input', (e) => {
     // debugger;
@@ -13,8 +14,12 @@ THEME_TOGGLE.addEventListener('input', (e) => {
 	if(isChecked) {
         BODY.classList.add('body-dark');
         BODY.classList.remove('body-light');
-        themeIcon.classList.add('fa-moon');
-        themeIcon.classList.remove('fa-sun');
+        THEME_ICON.classList.add('fa-moon');
+        THEME_ICON.classList.remove('fa-sun');
+        if (PAGE_HEADING != null) {
+            PAGE_HEADING.classList.add('heading-dark-theme');
+            PAGE_HEADING.classList.remove('heading-light-theme');
+        }
         for (var i = 0; i < articles.length; i++) {
             article = articles[i];
             article.classList.add('article-dark');
@@ -25,8 +30,8 @@ THEME_TOGGLE.addEventListener('input', (e) => {
             articleTitle.classList.add('article-title-dark');
             articleTitle.classList.remove('article-title-light');
         }
-        for (var i = 0; i < sidebarItems.length; i++) {
-            sidebarItem = sidebarItems[i];
+        for (var i = 0; i < SIDEBAR_ITEMS.length; i++) {
+            sidebarItem = SIDEBAR_ITEMS[i];
             sidebarItem.classList.add('sidebar-items-dark');
             sidebarItem.classList.remove('sidebar-items-light');
         }
@@ -34,8 +39,12 @@ THEME_TOGGLE.addEventListener('input', (e) => {
 	} else {
         BODY.classList.add('body-light');
         BODY.classList.remove('body-dark');
-        themeIcon.classList.add('fa-sun');
-        themeIcon.classList.remove('fa-moon');
+        THEME_ICON.classList.add('fa-sun');
+        THEME_ICON.classList.remove('fa-moon');
+        if (PAGE_HEADING != null) {
+            PAGE_HEADING.classList.add('heading-light-theme');
+            PAGE_HEADING.classList.remove('heading-dark-theme');
+        }
         for (var i = 0; i < articles.length; i++) {
             article = articles[i];
             article.classList.add('article-light');
@@ -46,8 +55,8 @@ THEME_TOGGLE.addEventListener('input', (e) => {
             articleTitle.classList.add('article-title-light');
             articleTitle.classList.remove('article-title-dark');
         }
-        for (var i = 0; i < sidebarItems.length; i++) {
-            sidebarItem = sidebarItems[i];
+        for (var i = 0; i < SIDEBAR_ITEMS.length; i++) {
+            sidebarItem = SIDEBAR_ITEMS[i];
             sidebarItem.classList.add('sidebar-items-light');
             sidebarItem.classList.remove('sidebar-items-dark');
         }
